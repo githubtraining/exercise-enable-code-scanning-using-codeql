45cddc352f779e71e81a2cde524e19a618999db0
mattdavis0351 <mattdavis0351@github.com>
2021-10-12 08:08:52 -0700

    initial file upload

--- .github/actions/local-action/lib/gradeLearner.js
+++ .github/actions/local-action/lib/gradeLearner.js
@@ -1,18 +1,40 @@
-const github = require("@actions/github");
-const core = require("@actions/core");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 module.exports = async () => {
-  const token = core.getInput("token");
-  const octokit = github.getOctokit(token);
+  const { GITHUB_WORKSPACE } = process.env;
+  // read a file from the workspace
+  const file = fs.readFileSync(
+    `${GITHUB_WORKSPACE}/codeql-analysis.yml`,
+    "utf8"
+  );
+
+  // parse the file with js-yaml
+  const workflowData = yaml.load(file);
+
+  const {
+    jobs: {
+      analyze: { steps },
+    },
+  } = workflowData;
+
+  const hasCheckoutRepository = steps.some((step) =>
+    step.uses.includes("actions/checkout")
+  );
+  const hasInitializeCodeQL = steps.some((step) =>
+    step.uses.includes("codeql-action/init")
+  );
+  const hasPerformAnalysis = steps.some((step) =>
+    step.uses.includes("codeql-action/analyze")
+  );
 
   try {
     //   Do some logic to verify the leaner understands
-
-    if (GOOD - RESULT) {
+    if (hasCheckoutRepository && hasInitializeCodeQL && hasPerformAnalysis) {
       return {
         reports: [
           {
-            filename: "",
+            filename: `${GITHUB_WORKSPACE}/codeql-analysis.yml`,
             isCorrect: true,
             display_type: "actions",
             level: "info",
@@ -29,14 +51,27 @@ module.exports = async () => {
       return {
         reports: [
           {
-            filename: "",
+            filename: `${GITHUB_WORKSPACE}/codeql-analysis.yml`,
             isCorrect: false,
             display_type: "actions",
             level: "warning",
             msg: `incorrect solution`,
             error: {
-              expected: "What we expecrted",
-              got: `What we got`,
+              expected:
+                "We expected your workflow contain a valid Repository Checkout Step, CodeQL Initalization Step and CodeQL Analyze Step.  Ensure the actions you are using in your worklow are accurately referenced!",
+              got: `\n${
+                hasCheckoutRepository
+                  ? "Correct Checkout action"
+                  : "Missing valid Checkout action in workflow"
+              }\n${
+                hasInitializeCodeQL
+                  ? "Correct CodeQL Initalization action"
+                  : "Missing valid CodeQL Initialization action in workflow"
+              }\n${
+                hasPerformAnalysis
+                  ? "Correct CodeQL Analyze action"
+                  : "Missing valid CodeQL Analyze action in workflow"
+              }`,
             },
           },
         ],
@@ -46,14 +81,14 @@ module.exports = async () => {
     return {
       reports: [
         {
-          filename: filename,
+          filename: `${GITHUB_WORKSPACE}/codeql-analysis.yml`,
           isCorrect: false,
           display_type: "actions",
           level: "fatal",
           msg: "",
           error: {
             expected: "",
-            got: "An internal error occurred.  Please open an issue at: https://github.com/githubtraining/exercise-remove-commit-history and let us know!  Thank you",
+            got: "An internal error occurred.  Please open an issue at: https://github.com/githubtraining/exercise-enable-code-scanning-using-codeql and let us know!  Thank you",
           },
         },
       ],
